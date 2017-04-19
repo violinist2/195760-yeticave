41a526cf5672bc66dcdf2551a8d69bd3d10fc9d4
violinist2 <nepoymaesh@yandex.ru>
2017-04-19 22:47:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -27,9 +27,33 @@ $user_avatar = 'img/user.jpg';
         <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
+<?php
+    
+if ($is_auth == 0) {
+ echo "<div class=\"user-menu__image\">
+    
+    <img src=\"$user_avatar\" width=\"40\" height=\"40\" alt=\"Пользователь\">
+  </div>
+  <div class=\"user-menu__logged\">
+    <p>$user_name</p>
+  </div>";
+ } 
+ else { 
+     echo '<ul class="user-menu__list">
+    <li class="user-menu__item">
+      <a href="#">Регистрация</a>
+    </li>
+    <li class="user-menu__item">
+      <a href="#">Вход</a>
+    </li>
+  </ul>';
+  }
+        
+    ?>    
+       
 
-        <!-- здесь должен быть PHP код для показа аватара пользователя -->
 
+       
         </nav>
     </div>
 </header>
