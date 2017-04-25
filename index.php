6a0f07f5651174d805b9d5ddc77ee1aca9c5855c
violinist2 <nepoymaesh@yandex.ru>
2017-04-25 16:08:41 +0300

    2проверка

--- index.php
+++ index.php
@@ -13,13 +13,54 @@ $now = time();
 
 // далее нужно вычислить оставшееся время до начала следующих суток и записать его в переменную $lot_time_remaining
 // ...
-$product_category = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+$product_category = [
+    'Доски и лыжи', 
+    'Крепления', 
+    'Ботинки', 
+    'Одежда', 
+    'Инструменты', 
+    'Разное'
+];
 //print_r($product_category);
 
-$items = ['2014 Rossignol District Snowboard' => ["Доски и лыжи", "10999", "img/lot-1.jpg"], 'DC Ply Mens 2016/2017 Snowboard' => ["Доски и лыжи", "159999", "img/lot-2.jpg"], 'Крепления Union Contact Pro 2015 года размер L/XL'=> ["Крепления","8000","img/lot-3.jpg"], 'Ботинки для сноуборда DC Mutiny Charocal' => ["Ботинки","10999","img/lot-4.jpg"], 'Куртка для сноуборда DC Mutiny Charocal' => ["Одежда", "7500", "img/lot-5.jpg"], 'Маска Oakley Canopy' => ["Разное","5400","img/lot-6.jpg"]];
-//echo "<pre>";
-//print_r($items);
-//echo "</pre>";
+$items = [
+    [
+    'itemsname' => '2014 Rossignol District Snowboard',
+    'category' => 'Доски и лыжи', 
+    'price' => '10999', 
+    'image' => 'img/lot-1.jpg'
+    ], 
+    [
+    'itemsname' => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' =>'Доски и лыжи',
+    'price' => '159999', 
+    'image' => 'img/lot-2.jpg'
+    ], 
+    [
+    'itemsname' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+    'category' => 'Крепления',
+    'price' => '8000',
+    'image' => 'img/lot-3.jpg'
+     ],
+    [
+    'itemsname' => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'Ботинки',
+    'price' => '10999',
+    'image' => 'img/lot-4.jpg'
+     ], 
+    [
+    'itemsname' => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'Одежда',
+    'price' => '7500', 
+    'image' => 'img/lot-5.jpg'
+    ],
+    [
+    'itemsname' => 'Маска Oakley Canopy',
+    'category' => 'Разное',
+    'price' => '5400', 
+    'image' => 'img/lot-6.jpg'
+    ]
+];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -84,47 +125,28 @@ $items = ['2014 Rossignol District Snowboard' => ["Доски и лыжи", "109
         <div class="lots__header">
             <h2>Открытые лоты</h2>
             <select class="lots__select">
-                <?php
+            <option>Все категории</option>
+                <?php foreach ($product_category as $value): ?>
+                <option><?=$value; ?></option> 
+               <?php endforeach; ?> 
                 
-                $product_category = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
-                $index = 0;
-
-while ($index < 6) {
-    print "<option>";
-    print $product_category[$index];
-    print "</option>";
-    $index = $index + 1;
-}
-                ?>
-               
-               
-               
-                <option>Все категории</option>
+                     
             </select>
         </div>
         <ul class="lots__list">
-           <?php        
-         foreach  ($items as $key => $val) {
-
-            // echo $key; // название
-            // echo $val[0]; // вид
-            // echo $val[1]; // цена
-            // echo $val[2]."<br>"; // картинка
-             
-        //    foreach  ($val as $innerkey => $rr) {
-            ?>
+           <?php foreach  ($items as $val):?>
             <li class="lots__item lot">
                
                 <div class="lot__image">
-                    <img src="<?=$val[2];?>" width="350" height="260" alt="<?=$val[0];?>">
+                    <img src="<?=$val['image'];?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$val[0];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="#"><?=$key;?></a></h3>
+                    <span class="lot__category"><?=$val['category'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="#"><?=$val['itemsname'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$val[1];?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$val['price'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             <?=$lot_time_remaining;?>
@@ -132,11 +154,10 @@ while ($index < 6) {
                     </div>
                 </div>
             </li>
+<?php endforeach; ?> 
+
 </ul>
-            <?php
-     
-         }
-            ?>
+ 
             
     </section>
 </main>
